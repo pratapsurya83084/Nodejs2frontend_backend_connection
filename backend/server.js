02f65fad0089648e170b61f16baac32e947bbde6
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2024-05-22 17:51:20 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,81 @@
+// const express=require('express')   //common js syntax
+import  express from 'express' //module js syntax is used when we need to change  in package.json "type":"module"
+ const app=express()
+
+//create a server
+//  app.get('/',(req,res)=>{
+//     res.send("server is ready")
+//  })
+
+//get a list of 5 jokes
+ app.get('/api/jokes',(req,res)=>{
+  const jokes=[
+    {
+        id:1,
+      title:'a 1st jokes',
+      content:"this is jokes"
+    },
+
+    {
+        id:2,
+        title:'a 2nd jokes',
+        content:"this is jokes"
+    },
+
+    {
+        id:3,
+        title:'a 3rd jokes',
+        content:"this is jokes"
+    },
+
+    {
+        id:4,
+        title:'a 4rt jokes',
+        content:"this is jokes"
+    },
+    {
+        id:5,
+        title:'a 5th jokes',
+        content:"this is jokes"
+    },
+  ]
+
+  res.send(jokes)
+
+ })
+
+const  port=process.env.PORT || 5000;  //port is required if not mention ,not run on server
+
+app.listen(port,()=>{
+    console.log(`server at http://localhost:${port}`);
+})
+
+// import express from "express";
+// const app = express();
+
+// app.get("/prataps/data", (req, res) => {
+//   const data = [
+//     {
+//       id: 1,
+//       name: "pratap",
+//       class: "sybcs",
+//     },
+//     {
+//       id: 2,
+//       name: "omkar",
+//       class: "sybcs",
+//     },
+//     {
+//       id: 3,
+//       name: "dhananjay",
+//       class: "sybcs",
+//     },
+//   ];
+
+//   res.send(data);
+// });
+// const port=process.env.PORT||2000;
+
+// app.listen(port,()=>{
+// console.log(`the server is running at http://localhost:${port}`);
+// })
---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios"; //axios is use to  fetch backend data
+
+function App() {
+  const [jokes, setjokes] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/jokes") //fetch this api  ,i can created before
+      .then((response) => {
+        setjokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  return (
+    <>
+      <h1>heloo pratap</h1>
+      <p>jokes:{jokes.length}</p>
+
+      {jokes.map((joke, index) => {
+        return (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
